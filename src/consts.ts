bc02dda13aa274fbe384c320bde0fb28b19493be
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-28 18:07:28 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_AUTHOR = 'Cheap Nightbot';
+export const SITE_TITLE = 'A Potato Blog ~';
+export const SITE_DESCRIPTION = 'a potatoes personal blog..';
